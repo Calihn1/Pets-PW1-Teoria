0cfea657c8535fe9fb842e578a5043922382d934
Emanuel Hilacondo <ehilacondob@unsa.edu.pe>
2024-12-09 17:46:34 +0000

    Editar formulario de la base de datos

---
+++ editPetForm.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+binmode STDOUT, ':utf8';
+
+print "Content-type: text/html\n\n";
+
+print <<'EOF';
+<form id="editPetForm" action="editPet.pl" method="POST">
+  <div class="form-group">
+  <label for="name">Name:</label>
+  <input type="text" class="form-control" id="name" name="name" placeholder="Required field to change name" required>
+  </div>
+  <div class="form-group">
+    <label for="owner">Owner:</label>
+    <input type="text" class="form-control" id="owner" name="owner" >
+  </div>
+  <div class="form-group">
+    <label for="sex">Sex:</label>
+    <select class="form-control" id="sex" name="sex" >
+      <option value="" disabled selected>Select sex</option>
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="species">Species:</label>
+    <input type="text" class="form-control" id="species" name="species" >
+  </div>
+  <div class="form-group">
+    <label for="birth">Birth:</label>
+    <input type="date" class="form-control" id="birth" name="birth" >
+  </div>
+  <div class="form-group">
+    <label for="death">Death (optional):</label>
+    <input type="date" class="form-control" id="death" name="death">
+  </div>
+  <button type="submit" class="btn btn-warning">Update</button>
+</form>
+EOF
