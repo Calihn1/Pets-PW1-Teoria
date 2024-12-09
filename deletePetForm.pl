35119ad3a66629eda5b632dd060e60a868e652c8
Emanuel Hilacondo <ehilacondob@unsa.edu.pe>
2024-12-09 21:19:22 +0000

    Se simplifico el delete por id

--- deletePetForm.pl
+++ deletePetForm.pl
@@ -11,32 +11,8 @@ print "Content-type: text/html\n\n";
 print <<'EOF';
 <form id="deletePetForm" action="deletePet.pl" method="POST">
   <div class="form-group">
-    <label for="name">Name:</label>
-    <input type="text" class="form-control" id="name" name="name" required>
-  </div>
-  <div class="form-group">
-    <label for="owner">Owner:</label>
-    <input type="text" class="form-control" id="owner" name="owner" >
-  </div>
-  <div class="form-group">
-    <label for="sex">Sex:</label>
-    <select class="form-control" id="sex" name="sex" >
-      <option value="" disabled selected>Select sex</option>
-      <option value="male">Male</option>
-      <option value="female">Female</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="species">Species:</label>
-    <input type="text" class="form-control" id="species" name="species" >
-  </div>
-  <div class="form-group">
-    <label for="birth">Birth:</label>
-    <input type="date" class="form-control" id="birth" name="birth" >
-  </div>
-  <div class="form-group">
-    <label for="death">Death (optional):</label>
-    <input type="date" class="form-control" id="death" name="death">
+    <label for="id">ID:</label>
+    <input type="text" class="form-control" id="id" name="id" required>
   </div>
   <button type="submit" class="btn btn-danger">Delete</button>
 </form>
